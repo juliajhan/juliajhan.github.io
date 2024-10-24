df35cdeb4f25859a642b228e625ad372f75e1185
Julia Han <jjh6283@rit.edu>
2024-10-23 20:52:51 -0400

    Changed icons

--- src/components/LinkBar.js
+++ src/components/LinkBar.js
@@ -6,17 +6,25 @@ import githubIcon from '../images/github-icon.png'
 import linkedinIcon from '../images/linkedin-icon.png'
 import emailIcon from '../images/email-icon.png'
 
+// const GithubIcon = () => (
+//     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" width="40" height="40">
+//         <path d="M...Z" />
+//     </svg>
+// );
+
+
 const LinkBar = () => {
     return (
         <div className="link-bar">
             <a href={resume} target="_blank">
-                <img src={resumeIcon} alt="Resume" className="icon"/>
+                <img src={resumeIcon} alt="Resume" className="icon scaled-icon"/>
             </a>
             <a href="https://linkedin.com/juliajhan/" target="_blank">
                 <img src={linkedinIcon} alt="LinkedIn" className="icon"/>
             </a>
             <a href="https://github.com/jjh6283" target="_blank">
                 <img src={githubIcon} alt="Github" className="icon"/>
+                {/* <GithubIcon /> */}
             </a>
             <a href="mailto:juliajooheehan@gmail.com" target="_blank">
                 <img src={emailIcon} alt="Email" className="icon"/>
